ad63cbcab394587a108bdb789fe42f6a7f4a531b
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-05-20 13:09:33 -0600

    rename styl to conform to core css generation changes

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,1 @@
-module.exports = ".bdsft-client .connection-status{position:absolute;top:13px;right:0}.bdsft-client .connection-status .icon{display:inline-block!important}.bdsft-client .connection-status .icon.success{color:#008000}.bdsft-client .connection-status .icon.alert{color:#f00}"
+module.exports = {"connectionstatus":".bdsft-client .connection-status{position:absolute;top:13px;right:0}.bdsft-client .connection-status .icon{display:inline-block!important}.bdsft-client .connection-status .icon.success{color:#008000}.bdsft-client .connection-status .icon.alert{color:#f00}"}
